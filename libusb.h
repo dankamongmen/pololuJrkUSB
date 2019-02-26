56933db895428dd6b8750290cd8f9698ac770189
nick black <dankamongmen@gmail.com>
2019-02-26 14:22:44 -0500

    FindACMDevice: use sysfs to look up acm control dev from libusb (closes #6)

--- libusb.h
+++ libusb.h
@@ -1,19 +1,21 @@
 #ifndef POLOLUJRKUSB_LIBUSB
 #define POLOLUJRKUSB_LIBUSB
 
+#include <string>
 #include <iostream>
 #include <libusb.h>
 
 namespace PololuJrkUSB {
 
 void LibusbVersion(std::ostream& s);
-void LibusbGetTopology(libusb_device* dev);
+void LibusbGetTopology(libusb_device* dev, int* bus, int* port);
 void JrkGetFirmwareVersion(libusb_device_handle* dev);
 void JrkGetSerialNumber(libusb_device_handle* dev,
                         const libusb_device_descriptor* desc);
 void LibusbGetConfig(std::ostream& s, libusb_device_handle* dev);
 void LibusbGetDesc(std::ostream& s, libusb_device_handle* dev,
                    const libusb_device_descriptor* desc);
+std::string FindACMDevice(int bus, int port);
 
 }
 
